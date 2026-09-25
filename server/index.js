72f7c296bffe2fa8f17ad5c5021bf054cfd253ca
meghana <meghanasirigibattula@gmail.com>
2026-09-25 21:31:04 +0530

    login added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
-
 const app = express();
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Mart API is running');
 });
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
 
+  if (username === 'meghana' && password === '1234') {
+    return res.json({
+      message: 'Login successful',
+    });
+  }
+
+  return res.status(401).json({
+    message: 'Invalid username or password',
+  });
+});
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
